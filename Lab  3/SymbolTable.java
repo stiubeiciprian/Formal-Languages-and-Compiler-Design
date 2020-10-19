80f32912f9c59110f54a1b8fee4c8180bf007281
Stiubei Ciprian <stiubeiciprian@gmail.com>
2020-10-19 20:27:30 +0300

    Add Lab3

---
+++ Lab  3/SymbolTable.java
@@ -0,0 +1,30 @@
+
+public class SymbolTable {
+    HashTable<String, Integer> hashTable;
+    public SymbolTable() {
+        this.hashTable = new HashTable<>(10);
+    }
+
+    /**
+     * Returns the position of a given token in the symbol table.
+     * @param token
+     */
+    public Integer pos(String token) {
+        if(hashTable.contains(token)) {
+            return hashTable.get(token);
+        }
+        return this.add(token);
+    }
+
+    /**
+     * Adds a given token to the symbol table.
+     * @param token
+     */
+    public Integer add(String token) {
+        Integer position = hashTable.getCurrentSize() + 1;
+        hashTable.setCurrentSize(position);
+
+        hashTable.add(token, position);
+        return position;
+    }
+}
